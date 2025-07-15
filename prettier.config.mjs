9682a65cded9130a4128fa28e625f74898c7ba6e
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-07-15 12:51:18 +0200

    feat: add Prettier configuration and update dependencies for code formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import('prettier').Config}
+ */
+const config = {
+  semi: false,
+  singleQuote: true,
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  importOrder: ["^node:*$", "<THIRD_PARTY_MODULES>", "^@/(.*)$", "^\\..*$"],
+  importOrderSortSpecifiers: true,
+};
+
+export default config;
