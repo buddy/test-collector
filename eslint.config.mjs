479694a93b6ed06243bf13ed1227e37b843e67f1
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-07-15 12:49:07 +0200

    chore: update dependencies in package.json
    - Added ESLint and related packages for improved code quality.
    - Updated peer dependencies to ensure compatibility with Cypress and Jest.
    - Removed duplicate entry for @types/node and added types for TypeScript.

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+
+import js from "@eslint/js";
+import eslintParserTypeScript from "@typescript-eslint/parser";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config([
+  {
+    ignores: ["node_modules/**/*", "dist/**/*", "*.config.mjs"],
+  },
+  {
+    files: ["**/*.{js,mjs,ts}"],
+    plugins: { js },
+    extends: [js.configs.recommended],
+  },
+  {
+    files: ["**/*.{js,mjs,ts}"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.{ts}"],
+    languageOptions: {
+      parser: eslintParserTypeScript,
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+]);
