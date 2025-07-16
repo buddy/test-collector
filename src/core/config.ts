e0d070f0ddfc6e17746ceed7ece6dd6e798182a0
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-07-16 10:29:41 +0200

    refactor: consolidate configuration properties and improve fallback logic in BuddyUnitTestCollectorConfig

--- src/core/config.ts
+++ src/core/config.ts
@@ -3,89 +3,98 @@ import { IncomingHttpHeaders } from 'http'
 import env from '@/utils/env'
 import { Logger } from '@/utils/logger'
 
-interface BuddyTestCollectorConfig {
-  utToken: string
-  apiBaseUrl: string
-  debugEnabled: boolean
-  runRefType: string
-  runCommit?: string
-  runBranch?: string
-  sessionId?: string
-  runRefName?: string
-  runHash?: string
-  runPreCommit?: string
-  buildUrl?: string
-}
-
-export class Config {
+export default class BuddyUnitTestCollectorConfig {
   static libraryName = '@buddy-works/test-collector'
   context: string
-  config: BuddyTestCollectorConfig
   logger: Logger
+  utToken: string
+  debugEnabled: boolean
+  apiBaseUrl: string
+  sessionId: string | undefined
+  runHash: string | undefined
+  runRefName: string | undefined
+  runRefType: string
+  runCommit: string | undefined
+  runPreCommit: string | undefined
+  runBranch: string | undefined
+  buildUrl: string | undefined
+
+  readonly #fallback = {
+    runRefType: 'BRANCH',
+
+    get apiBaseUrl() {
+      return env.BUDDY_UT_TOKEN.startsWith('bud_ut_eu') ? 'https://api.eu.buddy.works' : 'https://api.buddy.works'
+    },
+
+    get runBranch() {
+      // TODO: check if we're in git repo and git installed
+      try {
+        return execSync('git rev-parse --abbrev-ref HEAD', {
+          encoding: 'utf8',
+        }).trim()
+      } catch {
+        return undefined
+      }
+    },
+
+    get runCommit() {
+      // TODO: check if we're in git repo and git installed
+      try {
+        return execSync('git rev-parse HEAD', { encoding: 'utf8' }).trim()
+      } catch {
+        return undefined
+      }
+    },
+
+    get runPreCommit() {
+      // TODO: check if we're in git repo and git installed
+      try {
+        return execSync('git rev-parse HEAD', { encoding: 'utf8' }).trim()
+      } catch {
+        return undefined
+      }
+    },
+  }
 
   constructor(context: string) {
     this.context = context
-    this.logger = new Logger(`${Config.libraryName}_${context}`)
 
-    this.config = {
-      utToken: env.BUDDY_UT_TOKEN,
-      debugEnabled: env.BUDDY_LOGGER_DEBUG,
-      apiBaseUrl: env.BUDDY_API_URL ?? this.#fallbackApiBaseUrl,
-      sessionId: env.BUDDY_SESSION_ID,
-      runHash: env.BUDDY_RUN_HASH,
-      runRefName: env.BUDDY_RUN_REF_NAME,
-      runRefType: env.BUDDY_RUN_REF_TYPE ?? this.#fallbackRunRefType,
-      runCommit: env.BUDDY_RUN_COMMIT ?? this.#fallbackRunCommit,
-      runPreCommit: env.BUDDY_RUN_PRE_COMMIT ?? this.#fallbackRunPreCommit,
-      runBranch: env.BUDDY_RUN_BRANCH ?? this.#fallbackRunBranch,
-      buildUrl: env.BUDDY_RUN_URL,
-    }
+    const loggerName = `${BuddyUnitTestCollectorConfig.libraryName}_${context}`
+    this.logger = new Logger(loggerName)
 
-    this.logger.debug(`Config loaded in ${Config.libraryName}:`, this.config)
+    this.utToken = env.BUDDY_UT_TOKEN
+    this.debugEnabled = env.BUDDY_LOGGER_DEBUG
+    this.apiBaseUrl = env.BUDDY_API_URL ?? this.#fallback.apiBaseUrl
+    this.sessionId = env.BUDDY_SESSION_ID
+    this.runHash = env.BUDDY_RUN_HASH
+    this.runRefName = env.BUDDY_RUN_REF_NAME
+    this.runRefType = env.BUDDY_RUN_REF_TYPE ?? this.#fallback.runRefType
+    this.runCommit = env.BUDDY_RUN_COMMIT ?? this.#fallback.runCommit
+    this.runPreCommit = env.BUDDY_RUN_PRE_COMMIT ?? this.#fallback.runPreCommit
+    this.runBranch = env.BUDDY_RUN_BRANCH ?? this.#fallback.runBranch
+    this.buildUrl = env.BUDDY_RUN_URL
+
+    this.logger.debug(`Config loaded in ${BuddyUnitTestCollectorConfig.libraryName}`)
   }
 
-  // TODO: remove if unused
   get headers(): IncomingHttpHeaders {
     return {
       'content-type': 'application/json',
-      authorization: `Bearer ${this.config.utToken}`,
+      authorization: `Bearer ${this.utToken}`,
     }
   }
 
-  readonly #fallbackRunRefType = 'BRANCH'
-
-  get #fallbackApiBaseUrl() {
-    return env.BUDDY_UT_TOKEN.startsWith('bud_ut_eu')
-      ? 'https://api.eu.buddy.works'
-      : 'https://api.buddy.works'
-  }
-
-  get #fallbackRunBranch() {
-    // TODO: check if we're in git repo and git installed
-    try {
-      return execSync('git rev-parse --abbrev-ref HEAD', {
-        encoding: 'utf8',
-      }).trim()
-    } catch {
-      return undefined
+  get sessionPayload() {
+    const payload = {
+      ref_type: this.runRefType,
+      ref_name: this.runRefName ?? this.runBranch,
+      from_revision: this.runPreCommit,
+      to_revision: this.runCommit,
+      ...(this.runHash && { build_id: this.runHash }),
+      ...(this.buildUrl && { build_url: this.buildUrl }),
     }
-  }
 
-  get #fallbackRunCommit() {
-    // TODO: check if we're in git repo and git installed
-    try {
-      return execSync('git rev-parse HEAD', { encoding: 'utf8' }).trim()
-    } catch {
-      return undefined
-    }
-  }
-
-  get #fallbackRunPreCommit() {
-    // TODO: check if we're in git repo and git installed
-    try {
-      return execSync('git rev-parse HEAD', { encoding: 'utf8' }).trim()
-    } catch {
-      return undefined
-    }
+    this.logger.debug('Generated session payload:', payload)
+    return payload
   }
 }
