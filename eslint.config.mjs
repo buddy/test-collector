810642dccb21b48090995295b5730f87e7ee9366
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-07-22 17:13:51 +0200

    fix: update ESLint config to ignore examples directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
   {
-    ignores: ['node_modules/**/*', 'dist/**/*', '*.config.mjs'],
+    ignores: ['node_modules/**/*', 'examples/**/*', 'dist/**/*', '*.config.mjs'],
   },
   {
     files: ['**/*.{js,mjs,ts}'],
