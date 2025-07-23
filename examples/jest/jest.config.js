ef9a5c1eb2f22f61dbbbef5ce0156832ea7774e3
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-07-23 14:59:11 +0200

    fix: correct reporter configuration format in Jest config

--- examples/jest/jest.config.js
+++ examples/jest/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('jest').Config} */
 const config = {
-  reporters: ['default', ['@buddy-works/test-collector/jest']],
+  reporters: ['default', '@buddy-works/test-collector/jest'],
 }
 
 module.exports = config
