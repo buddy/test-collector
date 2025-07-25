910ff54a7c230980a54d6f5de025e3385a289837
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-07-25 16:20:00 +0200

    feat(jasmine): enhance setup reporter with additional logging for better visibility

--- examples/jasmine/__tests__/helpers/setup-reporter.js
+++ examples/jasmine/__tests__/helpers/setup-reporter.js
@@ -1,4 +1,10 @@
+console.log('Setting up Buddy test collector reporter...')
+
 const buddyTestCollector = require('@buddy-works/test-collector/jasmine').default
+
+console.log('Buddy test collector loaded:', typeof buddyTestCollector)
 
 // Register the reporter with jasmine
 jasmine.getEnv().addReporter(buddyTestCollector)
+
+console.log('Buddy test collector registered with Jasmine')
