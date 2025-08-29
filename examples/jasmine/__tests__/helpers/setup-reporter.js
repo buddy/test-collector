52ec1cac86808ca1d58181d8667d8174cac98b6c
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-08-29 14:48:17 +0200

    update jasmine spec folder to match the standard

--- examples/jasmine/__tests__/helpers/setup-reporter.js
+++
@@ -1,4 +0,0 @@
-const buddyTestCollector = require('@buddy-works/unit-tests/jasmine').default
-
-// Register the reporter with jasmine
-jasmine.getEnv().addReporter(buddyTestCollector)
---
+++ examples/jasmine/spec/helpers/setup-reporter.js
@@ -0,0 +1,1 @@
+jasmine.getEnv().addReporter(require('@buddy-works/unit-tests/jasmine').default)
