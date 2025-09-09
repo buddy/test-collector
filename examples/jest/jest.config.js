02299a11c930922b450d979a8473e834b5eca5cf
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-09-09 13:28:36 +0200

    chore(jest): remove comment from testTimeout configuration

--- examples/jest/jest.config.js
+++ examples/jest/jest.config.js
@@ -2,7 +2,7 @@
 const config = {
   reporters: ['default', '@buddy-works/unit-tests/jest'],
   maxWorkers: 4,
-  testTimeout: 10000, // 10 second timeout per test
+  testTimeout: 10000,
   cache: true,
   workerIdleMemoryLimit: '512MB',
 }
