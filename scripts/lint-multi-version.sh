2675af80e3d5df621230b02dd5f3887d9378cecf
Cyprian Zdebski <cyprian.zdebski@buddy.works>
2025-10-31 12:40:24 +0100

    feat(lint): add multi-version lint check to CI and update dependencies

--- scripts/lint-multi-version.sh
+++ scripts/lint-multi-version.sh
@@ -3,6 +3,16 @@ set -e
 
 FRAMEWORKS=(
   "vitest:3:4"
+
+  "@jest/reporters:29:30"
+  "@jest/types:29:30"
+
+  "@types/mocha:9:10"
+  "mocha:10:11"
+
+  "@playwright/test:1.55:1.56"
+
+  "cypress:14:15"
 )
 
 echo "Multi-version linting started"
